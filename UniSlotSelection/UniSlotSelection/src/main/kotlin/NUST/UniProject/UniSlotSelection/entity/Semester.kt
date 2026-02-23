51a5e50a7a1c8c65957d57d3d2bdb53f5a356ae3
Muhammad-Ibrahim-imp <m.ibrahim.imp@gmail.com>
2026-02-23 21:35:17 +0500

    Semester table added

--- UniSlotSelection/UniSlotSelection/src/main/kotlin/NUST/UniProject/UniSlotSelection/entity/Semester.kt
+++ UniSlotSelection/UniSlotSelection/src/main/kotlin/NUST/UniProject/UniSlotSelection/entity/Semester.kt
@@ -1,4 +1,14 @@
-//package NUST.UniProject.UniSlotSelection.entity
-//
-//class Semester {
-//}
+package NUST.UniProject.UniSlotSelection.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "semester")
+data class Semester (
+    @Id
+    @GeneratedValue(GenerationType.IDENTITY)
+    val semesterId: Long = 0,
+
+    val term: String = "",
+    val year: String = "",
+)
