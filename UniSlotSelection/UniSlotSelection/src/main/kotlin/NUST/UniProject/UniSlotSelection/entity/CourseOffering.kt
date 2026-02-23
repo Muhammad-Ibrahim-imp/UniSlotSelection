c0a04571a54bba05f44d52c0d212d37d05f23afe
Muhammad-Ibrahim-imp <m.ibrahim.imp@gmail.com>
2026-02-23 21:54:56 +0500

    All tables created

--- UniSlotSelection/UniSlotSelection/src/main/kotlin/NUST/UniProject/UniSlotSelection/entity/CourseOffering.kt
+++ UniSlotSelection/UniSlotSelection/src/main/kotlin/NUST/UniProject/UniSlotSelection/entity/CourseOffering.kt
@@ -1,4 +1,34 @@
-//package NUST.UniProject.UniSlotSelection.entity
-//
-//class CourseOffering {
-//}
+package NUST.UniProject.UniSlotSelection.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "course_offering")
+data class CourseOffering(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val offeringId: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    val course: Course,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "professor_id")
+    val professor: Professor,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    val department: Department,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "semester_id")
+    val semester: Semester,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "time_slot_id")
+    val timeSlot: TimeSlot,
+
+    val capacity: Int
+)
--- UniSlotSelection/UniSlotSelection/src/main/kotlin/NUST/UniProject/UniSlotSelection/entity/Enrollment.kt
+++ UniSlotSelection/UniSlotSelection/src/main/kotlin/NUST/UniProject/UniSlotSelection/entity/Enrollment.kt
@@ -3,11 +3,19 @@ package NUST.UniProject.UniSlotSelection.entity
 import jakarta.persistence.*
 
 
-//@Entity
-//@Table(name = "enrollment")
-//data class Enrollment (
-//
-//    @Id
-//    @GeneratedValue(strategy = Gene)
-//
-//)
+@Entity
+@Table(name = "enrollment")
+data class Enrollment(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val enrollmentId: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    val student: Student,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "offering_id")
+    val offering: CourseOffering
+)
