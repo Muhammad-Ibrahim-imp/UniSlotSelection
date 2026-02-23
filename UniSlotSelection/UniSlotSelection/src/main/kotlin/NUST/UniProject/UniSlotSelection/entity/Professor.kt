cb427e38958a6724a7833bda583054e644be3b3f
Muhammad-Ibrahim-imp <m.ibrahim.imp@gmail.com>
2026-02-23 21:40:31 +0500

    professor table added

--- UniSlotSelection/UniSlotSelection/src/main/kotlin/NUST/UniProject/UniSlotSelection/entity/Professor.kt
+++ UniSlotSelection/UniSlotSelection/src/main/kotlin/NUST/UniProject/UniSlotSelection/entity/Professor.kt
@@ -1,8 +1,17 @@
-//package NUST.UniProject.UniSlotSelection.entity
-//
-//import jakarta.persistence.*
-//
-//@Entity
-//@Table(name = "professor")
-//class Professor {
-//}
+package NUST.UniProject.UniSlotSelection.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "professor")
+data class Professor (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val professorId : Int = 0,
+
+    val professorName : String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    val department: Department
+    )
